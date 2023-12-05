35e76fa8027f61833a47902dc33aae6b281ecb66
rooney <bch3454@gmail.com>
2023-12-05 19:03:46 +0900

    Feat: afterLogin Layout 퍼블리싱

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,7 +1,21 @@
+import styles from "@/app/(afterLogin)/layout.module.css";
+
 export default function AfterLoginLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>AfterLoginLayout{children}</div>;
+  return (
+    <div className={styles.container}>
+      <header className={styles.leftSectionWrapper}>
+        <section className={styles.leftSection}></section>
+      </header>
+      <div className={styles.rightSectionWrapper}>
+        <div className={styles.rightSectionInner}>
+          <main className={styles.main}>{children}</main>
+          <section className={styles.rightSection}></section>
+        </div>
+      </div>
+    </div>
+  );
 }
